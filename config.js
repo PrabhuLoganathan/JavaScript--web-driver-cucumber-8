301d19bdbd6031f9768faecfc3ad5599b892173c
DEVINDER\Devinder <devinbhullar@gmail.com>
2017-02-16 20:26:19 +1100

    First commit

---
+++ config.js
@@ -0,0 +1,29 @@
+//protractor.conf.js
+exports.config = {
+  seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
+  getPageTimeout: 600000,
+  allScriptsTimeout: 5000000,
+  framework: 'custom',
+  // path relative to the current config file
+  frameworkPath: require.resolve('protractor-cucumber-framework'),
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  // Spec patterns are relative to this directory.
+  specs: [
+    'features/*.feature',
+  ],
+
+
+  baseURL: 'http://localhost:8080/',
+
+  cucumberOpts: {
+    //require: 'features/step_definitions/*.steps.js',
+    require: 'features/stepDefinitions.js',
+    tags: false,
+    format: 'pretty',
+    profile: false,
+    'no-source': true
+  }
+};
