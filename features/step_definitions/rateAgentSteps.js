49ba3e4564585d1dc9c7d232f7b1ab42457e8496
DEVINDER\Devinder <devinbhullar@gmail.com>
2017-03-01 16:38:27 +1100

    updated rateAgentSteps.js

--- features/step_definitions/rateAgentSteps.js
+++ features/step_definitions/rateAgentSteps.js
@@ -7,28 +7,28 @@ var rateAgentPage = function(){
   var homePage = new LandingPage();
 
       this.Given(/^I go to "([^"]*)"$/, function(site) {
-      // Write code here that turns the phrase above into concrete actions
+
         homePage.get(site);
     });
 
 
    this.Then(/^I should see the page title$/, function () {
-        // Write code here that turns the phrase above into concrete actions
+
         return expect(homePage.getTitle()).to.eventually.equal('Australia\'s No. 1 real estate agent ratings website | RateMyAgent');
       });
 
     this.Given(/^I click on the Search Button$/, function () {
-              return homePage.search();// Write code here that turns the phrase above into concrete actions
+              return homePage.search();
        });
 
 
       this.Given(/^I enter a "([^"]*)" name$/, function (locatename) {
-        // Write code here that turns the phrase above into concrete actions
+      
             return homePage.locationName(locatename);
     });
 
     this.Then(/^I should see the list of "([^"]*)" displayed$/, function (results) {
-        // Write code here that turns the phrase above into concrete actions
+
          var searchList = element.all(by.repeater('result in vm.resultSet.Results'));
          return expect(searchList.get(1).getText()).to.eventually.contain(results);
 
